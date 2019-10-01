f2dd42833bbb62e220b476e8e2585c2982d28452
Alexander "Ace" Olofsson <ace@haxalot.com>
2019-10-01 07:54:14 +0200

    Improve state management - and switching
    Now allows both popping the current state as well as moving the new
    state to the top of the stack when switching

--- src/StateManager.cpp
+++ src/StateManager.cpp
@@ -26,28 +26,33 @@ bool StateManager::hasState(const std::string& aName) const
     return false;
 }
 
-bool StateManager::pushState(std::unique_ptr<BaseState> aState, bool aSwitch)
+bool StateManager::pushState(std::unique_ptr<BaseState> aState, uint8_t aFlags)
 {
     auto name = aState->getName();
     aState->setApplication(m_application);
     aState->init();
 
-    m_states.push_back(std::move(aState));
+    m_states.push_front(std::move(aState));
 
-    if (aSwitch)
-        return changeState(name);
+    if ((aFlags & State_SwitchTo) == State_SwitchTo)
+        return changeState(name, aFlags);
     else
         return true;
 }
 
 bool StateManager::popState(const std::string& aName)
 {
-    for (size_t it = m_states.size() - 1; it >= 0; --it)
+    for (auto it = m_states.begin(); it != m_states.end(); ++it)
     {
-        auto& curState = m_states[it];
+        auto& curState = *it;
         if (curState->getName() == aName)
         {
-            m_states.erase(m_states.begin() + it);
+            m_curState = nullptr;
+            it = m_states.erase(it);
+
+            if (it != m_states.end())
+                m_curState = it->get();
+
             return true;
         }
     }
@@ -55,13 +60,27 @@ bool StateManager::popState(const std::string& aName)
     return false;
 }
 
-bool StateManager::changeState(const std::string& aName)
+bool StateManager::changeState(const std::string& aName, uint8_t aFlags)
 {
-    for (auto& state : Util::reverse(m_states))
+    for (auto& state : m_states)
     {
         if (state->getName() == aName)
         {
+            if ((aFlags & State_PopSelf) == State_PopSelf)
+            {
+                auto it = std::find_if(m_states.begin(), m_states.end(), [&](auto& test) { return test.get() == m_curState; });
+                m_states.erase(it);
+            }
+
             m_curState = state.get();
+
+            if ((aFlags & State_BringToFront) == State_BringToFront)
+            {
+                auto it = std::find(m_states.begin(), m_states.end(), state);
+                m_states.erase(it);
+                m_states.push_front(std::move(*it));
+            }
+
             return true;
         }
     }
--- src/StateManager.hpp
+++ src/StateManager.hpp
@@ -9,15 +9,23 @@ class BaseState;
 class StateManager
 {
 public:
+    enum ChangeFlags : uint8_t
+    {
+        State_None = 0,
+        State_SwitchTo     = 1 << 0,
+        State_BringToFront = 1 << 1,
+        State_PopSelf      = 1 << 2,
+    };
+
     StateManager();
     ~StateManager();
 
     void init(Application* aApplication);
 
     bool hasState(const std::string& aName) const;
-    bool pushState(std::unique_ptr<BaseState> aState, bool aSwitch = true);
+    bool pushState(std::unique_ptr<BaseState> aState, uint8_t aFlags = State_SwitchTo);
     bool popState(const std::string& aName);
-    bool changeState(const std::string& aName);
+    bool changeState(const std::string& aName, uint8_t aFlags = State_SwitchTo);
 
     inline BaseState* getCurrent() { return m_curState; }
     inline const BaseState* getCurrent() const { return m_curState; }
